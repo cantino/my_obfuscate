e2b459b5b049cd36cb7822eb7ed2f52cebb6341f
Sam Reh <samuelreh@gmail.com>
2013-03-15 17:51:28 -0700

    added postgres support

--- lib/my_obfuscate/postgres.rb
+++ lib/my_obfuscate/postgres.rb
@@ -1,22 +1,22 @@
 class MyObfuscate
-  class Mysql
+  class Postgres
     include MyObfuscate::DatabaseHelperShared
 
     def parse_insert_statement(line)
       if regex_match = insert_regex.match(line)
         {
             :table_name => regex_match[1].to_sym,
-            :column_names => regex_match[2].split(/`\s*,\s*`/).map { |col| col.gsub('`', "").to_sym }
+            :column_names => regex_match[2].split(/\s*,\s*/).map(&:to_sym)
         }
       end
     end
 
     def make_insert_statement(table_name, column_names, values_strings)
-      "INSERT INTO `#{table_name}` (`#{column_names.join('`, `')}`) VALUES #{values_strings};"
+      "INSERT INTO #{table_name} (#{column_names.join(', ')}) VALUES #{values_strings};"
     end
 
     def insert_regex
-      /^\s*INSERT INTO `(.*?)` \((.*?)\) VALUES\s*/i
+      /^\s*INSERT INTO (.*?) \((.*?)\) VALUES\s*/i
     end
 
   end
