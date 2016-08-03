f1edd7b9389ff6dabe1346356c055198420e4507
Guy Paddock <guy.paddock@redbottledesign.com>
2016-08-02 21:54:38 -0400

    Fixes calls to check config from statement parsers.

--- lib/my_obfuscate/insert_statement_parser.rb
+++ lib/my_obfuscate/insert_statement_parser.rb
@@ -7,11 +7,16 @@ class MyObfuscate
           table_name = table_data[:table_name]
           columns = table_data[:column_names]
           ignore = table_data[:ignore]
-          if config[table_name]
-            check_for_defined_columns_not_in_table(table_name, columns)
-            check_for_table_columns_not_in_definition(table_name, columns)
 
-            output_io.puts obfuscator.obfuscate_bulk_insert_line(line, table_name, columns, ignore)
+          if config[table_name]
+            obfuscator.check_for_defined_columns_not_in_table(
+              table_name, columns)
+
+            obfuscator.check_for_table_columns_not_in_definition(
+              table_name, columns)
+
+            output_io.puts obfuscator.obfuscate_bulk_insert_line(
+              line, table_name, columns, ignore)
           else
             obfuscator.handle_column_error("#{table_name} was not specified in the config. Please specify the table definition or set it to :keep.")
             output_io.write line
